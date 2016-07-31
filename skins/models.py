429d6ee2db826d7274137d811bcf3fed90bc9278
H-M-H <henry@freedesk.net>
2016-08-01 01:42:17 +0200

    upload skins to MEDIA_ROOT/skins

--- skins/models.py
+++ skins/models.py
@@ -1,21 +1,17 @@
-'''
-Models for the skindatabase.
-'''
+'''Models for the skindatabase.'''
 
 from datetime import datetime
 from django.db import models
 
 
 class Skin(models.Model):
-    '''
-    The model for a Teeworlds-Skin.
-    '''
+    '''The model for a Teeworlds-Skin.'''
 
     name = models.CharField(unique=True, max_length=23)
     creator = models.CharField(max_length=15)
     pack = models.CharField(max_length=64)
     release_date = models.DateTimeField(default=datetime.now)
-    skin_image = models.ImageField()
+    skin_image = models.ImageField(upload_to='skins')
 
     def __str__(self):
         '''Stringrepresentation of this model.'''
