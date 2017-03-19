c1f785f9d3dd66522b3c3b50223f424fa21ada80
HMH <henry@freedesk.net>
2017-03-19 03:31:11 +0100

    improved admininterface

--- maps/admin.py
+++ maps/admin.py
@@ -1,9 +1,27 @@
-from django.conf.urls import url
+from django.contrib.admin import ModelAdmin
 
 from ddnet_django import admin
 from .models import MapRelease, ServerType, Map
 
 
+class MapAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'mapper',
+        'server_type',
+        'stars',
+        'timestamp'
+    )
+
+    list_filter = (
+        'server_type',
+    )
+    search_fields = (
+        'name',
+        'mapper',
+    )
+
+
 admin.site.register(MapRelease)
 admin.site.register(ServerType)
-admin.site.register(Map)
+admin.site.register(Map, MapAdmin)
--- skins/admin.py
+++ skins/admin.py
@@ -1,9 +1,29 @@
 '''Models are registered here to provide access for the admininterface.'''
 
 from ddnet_django import admin
+from django.contrib.admin import ModelAdmin
 
 
 from .models import Skin
 
 
-admin.site.register(Skin)
+class SkinAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'creator',
+        'pack',
+        'release_date'
+    )
+
+    list_filter = (
+        'pack',
+        'creator'
+    )
+    search_fields = (
+        'name',
+        'creator',
+        'pack',
+    )
+
+
+admin.site.register(Skin, SkinAdmin)
