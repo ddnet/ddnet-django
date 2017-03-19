28062f08603605b0d00d1b6335c7ed54f8a7a3cb
HMH <henry@freedesk.net>
2017-03-19 03:08:43 +0100

    fixed problem django had with an unbound function

--- ddnet_base/validtors.py
+++ ddnet_base/validtors.py
@@ -1,15 +1,21 @@
 from django.core.exceptions import ValidationError
 from django.core.files.images import get_image_dimensions
 
+from django.utils.deconstruct import deconstructible
 
-def image_validator(width, height):
-    def img_val(img):
+
+@deconstructible
+class image_validator:
+
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def __call__(self, img):
         w, h = get_image_dimensions(img)
 
-        if w != width:
-            raise ValidationError('Image must be {} px wide, not {} px.'.format(width, w))
+        if w != self.width:
+            raise ValidationError('Image must be {} px wide, not {} px.'.format(self.width, w))
 
-        if h != height:
-            raise ValidationError('Image must be {} px high, not {} px.'.format(height, h))
-
-    return img_val
+        if h != self.height:
+            raise ValidationError('Image must be {} px high, not {} px.'.format(self.height, h))
