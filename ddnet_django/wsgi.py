c2601588c9551974c05c61d52fdb55583771c277
HMH <henry@freedesk.net>
2017-04-07 22:58:37 +0200

    better place to init release and fix threads hopefully
    like this these threads should be started only once, even during
    developement unlike previously as manage.py runservers tends to execute
    AppConfig.ready twice
    additionally this avoids starting these threads while calling management
    commands

--- ddnet_django/wsgi.py
+++ ddnet_django/wsgi.py
@@ -14,3 +14,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddnet_django.settings")
 
 application = get_wsgi_application()
+
+
+from threading import Thread
+from maps.utils import handle_scheduled_releases, handle_cleanup
+t = Thread(target=handle_scheduled_releases)
+t.daemon = True
+t.start()
+
+t2 = Thread(target=handle_cleanup)
+t2.daemon = True
+t2.start()
--- maps/apps.py
+++ maps/apps.py
@@ -1,5 +1,3 @@
-from threading import Thread
-
 from django.apps import AppConfig
 
 class MapsConfig(AppConfig):
@@ -7,12 +5,4 @@ class MapsConfig(AppConfig):
 
     def ready(self):
         import maps.signals
-
-        from maps.utils import handle_scheduled_releases, handle_cleanup
-        t = Thread(target=handle_scheduled_releases)
-        t.daemon = True
-        t.start()
-
-        t2 = Thread(target=handle_cleanup)
-        t2.daemon = True
-        t2.start()
+        pass
